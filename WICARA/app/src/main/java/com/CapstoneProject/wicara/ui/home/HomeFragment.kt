02b638dc686271cf12b140cf7b10d5219958c3c5
M Faezol Padli <53302125+faizul14@users.noreply.github.com>
2022-05-25 00:14:28 +0800

    Revert "Revert "Md contributor""

--- WICARA/app/src/main/java/com/CapstoneProject/wicara/ui/home/HomeFragment.kt
+++ WICARA/app/src/main/java/com/CapstoneProject/wicara/ui/home/HomeFragment.kt
@@ -1,5 +1,7 @@
 package com.CapstoneProject.wicara.ui.home
 
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -37,6 +39,28 @@ class HomeFragment : Fragment() {
         binding.cardView1.setOnClickListener {
             Toast.makeText(requireActivity(), "ini di klik", Toast.LENGTH_SHORT).show()
         }
+        playAnimation()
+        return root
+    }
+
+    private fun playAnimation(){
+
+        val txt1 = ObjectAnimator.ofFloat(binding!!.textView14, View.ALPHA, 1f).setDuration(1000)
+        val txt2 = ObjectAnimator.ofFloat(binding!!.txtUser, View.ALPHA, 1f).setDuration(1000)
+        val cd1 = ObjectAnimator.ofFloat(binding!!.cardView1, View.ALPHA, 1f).setDuration(500)
+        val cd2 = ObjectAnimator.ofFloat(binding!!.cardView2, View.ALPHA, 1f).setDuration(500)
+        val cd3 = ObjectAnimator.ofFloat(binding!!.cardView3, View.ALPHA, 1f).setDuration(500)
+        val cd4 = ObjectAnimator.ofFloat(binding!!.cardView4, View.ALPHA, 1f).setDuration(500)
+
+        val text = AnimatorSet().apply {
+            playTogether(txt1, txt2)
+        }
+        AnimatorSet().apply {
+            playSequentially(text, cd1, cd2, cd3, cd4)
+            start()
+        }
+
+    }
         return root
     }
 
