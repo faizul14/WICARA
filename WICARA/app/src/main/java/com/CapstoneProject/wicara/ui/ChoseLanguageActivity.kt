c9e1cd3e2b035419b78c7f57780e24e64dc96dc7
faizul14 <fpcyber61@gmail.com>
2022-05-27 21:59:38 +0800

    fix fitur text to text and add fitur chose language

---
+++ WICARA/app/src/main/java/com/CapstoneProject/wicara/ui/ChoseLanguageActivity.kt
@@ -0,0 +1,12 @@
+package com.CapstoneProject.wicara.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.CapstoneProject.wicara.R
+
+class ChoseLanguageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chose_language)
+    }
+}
