341153b45d87ad80aefe8d1d0aa6def0e7b1532a
faizul14 <fpcyber61@gmail.com>
2022-06-11 09:43:54 +0800

    fix autentikai login firebase

--- WICARA/app/src/main/java/com/CapstoneProject/wicara/MainActivity2.kt
+++ WICARA/app/src/main/java/com/CapstoneProject/wicara/MainActivity2.kt
@@ -1,5 +1,6 @@
 package com.CapstoneProject.wicara
 
+import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.view.WindowInsets
@@ -11,16 +12,32 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.CapstoneProject.wicara.databinding.ActivityMain2Binding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity2 : AppCompatActivity() {
 
     private lateinit var binding: ActivityMain2Binding
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMain2Binding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        //for ceheck sesi login with firebase
+        auth = Firebase.auth
+        val firebaseUser = auth.currentUser
+        if (firebaseUser == null) {
+            // Not signed in, launch the Login activity
+            startActivity(Intent(this, AutentikasiLoginActivity::class.java))
+            finish()
+            return
+        }
+
+
 
         val navView: BottomNavigationView = binding.navView
 
